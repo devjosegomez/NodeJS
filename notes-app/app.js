87deebb216c88dcc0b295871bdeba44b9bf80959
devjosegomez <devjosegomez@gmail.com>
2020-02-12 10:38:42 -0600

    Common usage of fs (file system) build-in library. -writeFileSync (creates a file) -appendFileSync (append text without deleting the existing text)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,11 @@
 //  Creating files using fs (File System) with NodeJS
 //  https://nodejs.org/dist/latest-v12.x/docs/api/fs.html#fs_fs_writefilesync_file_data_options
 
-//import library fs
+// import library fs
 const fs = require('fs')
 
+// Create a file in this case note.txt
 fs.writeFileSync("notes.txt", "Hey, this file was created by NodeJs")
+
+// Appending text to a file 
+fs.appendFileSync("notes.txt", "\nThis text was appended using appendFileSync method")
