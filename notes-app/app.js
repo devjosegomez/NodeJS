e2648d96f52ddec125a80089565581324bcb7ec8
devjosegomez <devjosegomez@gmail.com>
2020-02-14 16:21:29 -0600

    yargs.command [add, read, remove, list]

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
+/*  This app allow us to create, delete and read all the notes  */
+
 //imports
 //  Creating files using fs (File System) with NodeJS
 //  https://nodejs.org/dist/latest-v12.x/docs/api/fs.html#fs_fs_writefilesync_file_data_options
@@ -8,18 +10,54 @@ const getNotes = require('./notes.js')
 const validator = require('validator')
 //chalk.js Terminal string styling done right
 const chalk = require('chalk')
+const yargs = require('yargs')
+
+//Set version of the app
+yargs.version('1.0.0')
+
+//[Add] command
+yargs.command({
+    command: 'add',                     //Command name
+    describe: 'Add a new note',         //Description of the command
+    handler: () =>{                     //Funtion that will execute that command
+        console.log('Adding a new note!!!')
+    }
+})
+
+//[Remove] command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () =>{
+        console.log('Removing a note!')
+    }
+})
+
+//[List all] command
+yargs.command({
+    command: 'list',
+    describe: 'List all the notes available',
+    handler: () =>{
+        console.log('Here is all your notes!')
+    }
+})
+
+//[Read] command
+yargs.command({
+    command: 'read',
+    describe: 'Read an indivual note by index',
+    handler: () =>{ 
+        console.log('Reading a note!')
+    }
+})
 
 //Get command from terminal as an argument
 const cmd = process.argv[2]
 console.log(cmd)
+//Get args vector with yargs
+console.log(yargs.argv)
 
-let msgCmd = ""
 
-if(cmd === 'add'){
-    msgCmd = "Adding note!"
-}else if(cmd === 'delete'){
-    msgCmd = "Deleting note!"
-}
 
 
 
